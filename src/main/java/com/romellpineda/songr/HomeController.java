dc9b7f0bcee41e390994d9d1797e1fac2050d2c9
Romell Pineda <romell.pineda@gmail.com>
2020-01-20 22:03:50 -0800

    add controller

---
+++ src/main/java/com/romellpineda/songr/HomeController.java
@@ -0,0 +1,21 @@
+package com.romellpineda.songr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getName(String str, Model m) {
+        m.addAttribute("username", "roml");
+        return "home";
+    }
+
+//    @GetMapping("/emotions")
+//    public String getEmotions(Model m) {
+//
+//        return "emotions";
+//    }
+}
