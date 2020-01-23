f409e2dd8c3c10d58ca9a34869a54d5be7748be5
Romell Pineda <romell.pineda@gmail.com>
2020-01-23 13:27:37 -0800

    song class

---
+++ src/main/java/com/romellpineda/songr/Song.java
@@ -0,0 +1,15 @@
+package com.romellpineda.songr;
+
+public class Song {
+    String title;
+    int length;
+    int trackNumber;
+
+    public Song() {}
+
+    public Song(String title, int length, int trackNumber) {
+        this.title = title;
+        this.length = length;
+        this.trackNumber = trackNumber;
+    }
+}
